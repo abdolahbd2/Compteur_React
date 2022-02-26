87363889722275d12bb626cbe051efbdb4db54d5
abdolahbd <abdolahbd@gmail.com>
2022-02-26 12:50:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import {Container, Button, Input, Col,Row } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState,useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+
+const [count,setCount]=useState(0);
+
+  useEffect(() => {
+    if (count<0) {
+      setCount(0)
+    }
+  },[count]
+  
+  );
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className='container'>
+  <Row>   
+  <Col xs='2' xl='1' className='text-center'>
+    <Button color='primary' onClick={()=>setCount(count+1)}>Plus</Button>
+  </Col> 
+  
+  <Col xs='4'  >
+  <Row>
+    <Input className='text-center' value={count} onChange={()=>{}}></Input>
+  </Row>
+  <Row >
+    <Col  className='text-center m-3'>
+    <Button  color='success' onClick={()=>setCount(0)}>Reset</Button>
+  </Col>
+  </Row>
+  </Col> 
+  
+  <Col xs='2' xl='1' className='text-center'>
+    <Button  color='danger' onClick={()=>setCount(count-1)}>Moins</Button>
+  </Col> 
+  
+  </Row>
+         
+    </Container>
+   
   );
 }
+
+
 
 export default App;
